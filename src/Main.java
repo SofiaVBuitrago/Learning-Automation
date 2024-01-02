6afc64764a069bf58dd311c20e2a70fc2e00389d
Sofia Buitrago <sofia.buitrago@yopmail.com>
2024-01-02 15:00:56 -0500

    this is the first version of my project jeje

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Multiplicacion (5,4));
+    }
+
+
+    public static int Multiplicacion (int a, int b){
+        int c = 0;
+
+        if(b == 0 ){
+
+            return 0;
+        }else {
+            if (b==1){
+
+                return a;
+
+            }else{
+
+                c = a + Multiplicacion(a,b-1);
+
+            }
+
+        }
+        return c;
+
+    }
+            System.out.println("V1");
+}
